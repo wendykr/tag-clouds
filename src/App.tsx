517479a819daa6721332b5bf320d43912324a04d
Vendula Krajíčková <vendysacek@seznam.cz>
2024-04-26 06:25:49 +0200

    create tagsStructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import './App.css'
+import { useState } from 'react';
+import './App.scss';
+import { tagsData } from './constants/tags';
+import { CiCloudOn } from "react-icons/ci";
+
+const tagsStructure: string[] = tagsData;
 
 function App() {
+  const [tags, setTags] = useState<string[]>(tagsStructure);
+
+  console.log(tags);
 
   return (
     <>
-      
+      <div className="container">
+        <div className="head">
+          <CiCloudOn className="icon-cloud" /> <span className="title">tag cloud</span>
+        </div>
+        <div className="main">
+          {
+            tags.map((tag, index: number) => <p className="tag" key={index}>{tag}</p>)
+          }
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/constants/tags.ts
@@ -0,0 +1,1 @@
+export const tagsData = ['Workout Gear', 'Digital Art', 'Replacement', 'Aria', 'Fitness', 'Wireless', 'iWatch', 'Hike', 'Sportsband', 'Active', 'Medical', 'Free', 'Setup', 'Goal', 'Black', 'Activity', 'Dolor'];
